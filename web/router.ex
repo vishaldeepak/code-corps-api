c098055348b91e742af13398090697d2ab7922e8
npendery <npendery@gmail.com>
2017-04-28 13:54:38 -0600

    Update github action from create to handle

--- web/router.ex
+++ web/router.ex
@@ -55,8 +55,8 @@ defmodule CodeCorps.Router do
 
     post "/webhooks/stripe/connect", StripeConnectEventsController, :create
     post "/webhooks/stripe/platform", StripePlatformEventsController, :create
-    post "/webhooks/github_issues", GithubIssueController, :create
-    post "/webhooks/github_issue_comments", GithubIssueCommentController, :create
+    post "/webhooks/github_issues", GithubIssueController, :handle
+    post "/webhooks/github_issue_comments", GithubIssueCommentController, :handle
   end
 
   scope "/", CodeCorps, host: "api." do
